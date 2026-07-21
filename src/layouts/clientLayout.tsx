3d71593096897610f46a789f58a5c06422c0f57f
Aimen-Lebdi <aimen.lebdi@etu.univ-batna2.dz>
2026-07-21 17:32:53 +0100

    add ScrollToTop component for automatic scroll on route change

--- src/layouts/clientLayout.tsx
+++ src/layouts/clientLayout.tsx
@@ -1,11 +1,13 @@
 import { Header } from '../components/client/clientLayout/Header';
 import { Footer } from '../components/client/clientLayout/Footer';
 import { Outlet } from 'react-router-dom';
+import ScrollToTop from '../components/ScrollToTop';
 
 
 const ClientLayout = () => {
   return (
     <div className="min-h-screen bg-background flex flex-col">
+      <ScrollToTop />
       <Header />
       <main className="flex-1">
         <Outlet></Outlet>
